bfd78511d70b4c5ba8f73f1b4fa56171f0f6a6be
Christoph.Wolff <christoph.wolff@holisticon.de>
2019-03-08 12:55:23 +0100

    not needed

--- web/src/Main.tsx
+++ web/src/Main.tsx
@@ -1,13 +1,11 @@
 import * as React from "react";
 
-import { Grid, Paper, Fab } from "@material-ui/core";
-import AddIcon from '@material-ui/icons/Add';
+import { Grid, Paper } from "@material-ui/core";
 import { createStyles, withStyles } from "@material-ui/core/styles";
 import { Theme } from "@material-ui/core/styles/createMuiTheme";
 import TaskList from "./task-list/TaskList";
 import { TaskItem } from "./shared/interfaces";
 import TaskItemEditor from "./taks-editor/TaskItemEditor";
-import NewRequestDialog from "./dialogs/NewRequestDialog";
 
 
 export interface IMainProps {
@@ -16,37 +14,24 @@ export interface IMainProps {
 
 export interface IMainState {
   selectedTaskItem?: TaskItem;
-  open: boolean;
-  message: string;
 }
 
 const styles = (theme: Theme) =>
   createStyles({
-    fab: {
-      position: 'absolute',
-      top: theme.spacing.unit * 2,
-      right: theme.spacing.unit * 2,
-    },
+    
   });
 
 class Main extends React.Component<IMainProps, IMainState> {
   constructor(props: IMainProps) {
     super(props);
     this.state = {
-      selectedTaskItem: undefined,
-      open: false,
-      message: ""
+      selectedTaskItem: undefined
     };
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick = (taskItem: TaskItem) => (event: any) => {
     this.setState({ selectedTaskItem: taskItem })
   }
-
-  openNewRequestDialog = (event: any) => {
-    this.setState({ open: true });
-  }
-
 
 
   public render() {
@@ -62,24 +47,13 @@ class Main extends React.Component<IMainProps, IMainState> {
             <TaskList handleClick={this.handleClick} />
           </Grid>
           <Grid item xs={9}>
-            <Fab size="small"
-              color="primary"
-              aria-label="Add"
-              className={classes.fab}
-              onClick={this.openNewRequestDialog}
-            >
-              <AddIcon />
-            </Fab>
             {this.state && this.state.selectedTaskItem ? (
-
               <TaskItemEditor taskItem={this.state.selectedTaskItem} />
             ) : (
-
                 <Paper >select a task, nplease</Paper>
               )}
           </Grid>
         </Grid>
-        <NewRequestDialog open={this.state.open} />
       </React.Fragment>
     );
   }
